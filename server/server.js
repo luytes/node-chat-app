1251525e6dc5154de555932e286f4b049f251c7a
wenqingkevin ma <wenqing.kevin@gmail.com>
2019-01-12 22:38:08 +0100

    added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -4,12 +4,25 @@ const express = require('express');
 const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server); // this "io" is our server!
+
 app.use(express.static(publicPath));
 io.on('connection', (socket) => { // server and client keep chanel for as long as client want to
   console.log('New user connected');
+  // EMIT EVENTS
+  socket.emit('newMessage', {
+    from: 'Kevin',
+    text: 'Hey whats up',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (message) => { // this callback is the listener
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log("User was disconnected")
   });
@@ -17,4 +30,5 @@ io.on('connection', (socket) => { // server and client keep chanel for as long a
 server.listen(port, () => {
   console.log(`Server is up at port ${port}`);
 });
+
 
