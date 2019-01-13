e2af97ae0219db93b5c9d1723cef24bf622319d6
wenqingkevin ma <wenqing.kevin@gmail.com>
2019-01-13 20:43:18 +0100

    emit newMessage on createMessage and greet new user and alert others

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function () {
   console.log("Connected to server ");
   // EMIT EVENT
-  socket.emit('createMessage', {
-    from: 'Kevin',
-    text: 'Hey Lisa!'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'Kevin',
+  //   text: 'Hey Lisa!'
+  // });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -12,15 +12,37 @@ var io = socketIO(server); // this "io" is our server!
 app.use(express.static(publicPath));
 io.on('connection', (socket) => { // server and client keep chanel for as long as client want to
   console.log('New user connected');
+
   // EMIT EVENTS
-  socket.emit('newMessage', {
-    from: 'Kevin',
-    text: 'Hey whats up',
-    createdAt: 123
+  // socket.emit('newMessage', { // emits to a single connection
+  //   from: 'Kevin',
+  //   text: 'Hey whats up',
+  //   createdAt: 123
+  // });
+  socket.emit('newMessage', { // socket.emit from admin to the chat app
+    from: "Admin",
+    text: "Welcome to the Chat App!"
+  });
+
+  socket.broadcast.emit('newMessage', { // socket.broadcast.emit from admin to text new user joined
+    from: "Admin",
+    text: "New User Joined!",
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', (message) => { // this callback is the listener
     console.log('createMessage', message);
+
+    io.emit('newMessage', { // emits to every single connection
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', { // emit to everybody but this socket
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
