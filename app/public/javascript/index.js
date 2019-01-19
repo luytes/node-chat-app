269589e0535cebc096317de92f7c6dce600baa81
wenqingkevin ma <wenqing.kevin@gmail.com>
2019-01-19 23:43:54 +0100

    added mustache.js for message templates without npm and styled the chat messages

--- app/public/javascript/index.js
+++ app/public/javascript/index.js
@@ -15,21 +15,36 @@ socket.on('disconnect', function () {
 // LISTENER
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log('NewMessage', message);
-  var li = jQuery('<li><li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html(); // html returns the markup inside message-template which is the paragraph <p>
+  // the thing we add to browser
+  var html = Mustache.render(template, { // set it to mustache call, takes template we wanna render
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html); // renders it
+  // console.log('NewMessage', message);
+  // var li = jQuery('<li><li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
   // console.log('NewLocationMessage', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li><li>');
-  var a = jQuery('<a target="_blank">My current location<a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, { // set it to mustache call, takes template we wanna render
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html); // renders it
+  // var li = jQuery('<li><li>');
+  // var a = jQuery('<a target="_blank">My current location<a>');
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 });
 
 // socket.emit('createMessage', {
